e6e61abb1e7088f0c7fb506c8ebfb1be62bd7754
Victor Ly <ly.victor2000@gmail.com>
2023-09-15 13:04:38 +0200

    implemented api's

--- script.js
+++ script.js
@@ -9,13 +9,16 @@ const API_KEY = {
 }
 */
 
+// https://www.npmjs.com/package/latlon-geohash
+import Geohash from 'https://cdn.jsdelivr.net/npm/latlon-geohash@2.0.0' // latitude longitude converter for geoPoint query parameter for TicketMaster
+
 // initiate function on window load
 document.addEventListener("DOMContentLoaded", () => {
 
-  const input = { "query": "", "latitude": 0, "longitude": 0 };
+  const input = { "query": "", "latitude": 0, "longitude": 0, "geohash": "" };
   const output = {
-    "openweathermap": [],
-    "mapbox": [],
+    "openweathermap": {},
+    "mapbox": "",
     "ticketmaster": []
   };
 
@@ -23,26 +26,87 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", submitForm);
 
   // on form submit
-  function submitForm(e) {
+  async function submitForm(e) {
     e.preventDefault();
     
     input.query = this.search.value; // input value from the form
     //console.log(input.query);
 
-    getGeoCoordinates(input.query);
+    await convertGeoCoordinates(input.query);
+
+    convertGeohash(input.latitude, input.longitude);
+
+    await getAllData(input); // wait for the promise to resolve in convertGeoCoordinates
   }
 
   // convert to latitude and longitude values using OpenWeatherMap api
-  async function getGeoCoordinates(query) {
+  async function convertGeoCoordinates(query) {
     try {
       const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=${API_KEY.OPENWEATHERMAP}`, { method: "GET" });
-      const [obj] = await response.json(); // destructuring array
+
+      const [obj] = await response.json();
 
       input.latitude = obj.lat;
       input.longitude = obj.lon;
+
     } catch (error) {
       console.error(error);
     }
+  }
+
+  // geoPoint query parameter is more reliable than the city query parameter to search for events
+  function convertGeohash(latitude, longitude) {
+    input.geohash = Geohash.encode(latitude, longitude, 3); // (latitude, longitude, precision)
+  }
+
+  // get data from OpenWeatherMap
+  async function getOpenWeatherMapData({ latitude, longitude }) {
+    try {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY.OPENWEATHERMAP}`, { method: "GET" });
+      const data = await response.json();
+
+      return data;
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // get image url from MapBox
+  function getMapBoxData({ latitude, longitude }) {
+    try {
+      return `https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/${longitude},${latitude},8,0/360x280?access_token=${API_KEY.MAPBOX}`;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // get data from TicketMaster
+  async function getTicketMasterData({ geohash }) {
+    try {
+      const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?size=10&geoPoint=${geohash}&apikey=${API_KEY.TICKETMASTER}&sort=distance,date,asc`, { method: "GET" });
+      const data = await response.json();
+
+      const filteredData = data._embedded.events;
+
+      return filteredData;
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // get all data from the api's
+  async function getAllData(input) {
+    output.openweathermap = await getOpenWeatherMapData(input);
+    output.mapbox = getMapBoxData(input);
+    output.ticketmaster = await getTicketMasterData(input);
+
+    displayData(output);
+  }
+
+  function displayData() {
+
   }
 
 });
